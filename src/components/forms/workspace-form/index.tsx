9fe61106ca02de59253f49a265d2db456e3d6dea
s3xmaxman <badside3919@gmail.com>
2024-12-23 13:39:08 +0900

    Update WorkspaceForm to utilize useCreateWorkspace hook and add @hookform/error-message dependency
    - Integrated useCreateWorkspace hook in WorkspaceForm for improved form handling.
    - Updated package.json and package-lock.json to include @hookform/error-message dependency for enhanced error messaging in forms.

--- src/components/forms/workspace-form/index.tsx
+++ src/components/forms/workspace-form/index.tsx
@@ -1,7 +1,11 @@
+import { useCreateWorkspace } from "@/hooks/useCreateWorkspace";
 import React from "react";
 
 const WorkspaceForm = () => {
-  return <div>WorkspaceForm</div>;
+  const { errors, isPending, onFormSubmit, register } = useCreateWorkspace();
+  return (
+    <form onSubmit={onFormSubmit} className="flex flex-col gap-y-3"></form>
+  );
 };
 
 export default WorkspaceForm;
