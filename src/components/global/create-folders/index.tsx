d306cd82cdc356391b2925abab0d4a744a72680e
s3xmaxman <badside3919@gmail.com>
2024-12-23 16:46:28 +0900

    Refactor CreateFolders component to enable folder creation functionality
    - Uncommented and implemented the folder creation logic using the useCreateFolders hook.
    - Updated button click handling to trigger folder creation, enhancing user interaction for workspace folder management.

--- src/components/global/create-folders/index.tsx
+++ src/components/global/create-folders/index.tsx
@@ -1,16 +1,17 @@
 "use client";
+
 import FolderPlusDuotine from "@/components/icons/folder-plus-duotone";
 import { Button } from "@/components/ui/button";
-// import { useCreateFolders } from "@/hooks/useCreateFolders";
+import { useCreateFolders } from "@/hooks/useCreateFolders";
 import React from "react";
 
 type Props = { workspaceId: string };
 
 const CreateFolders = ({ workspaceId }: Props) => {
-  //   const { onCreateNewFolder } = useCreateFolders(workspaceId);
+  const { onCreateNewFolder } = useCreateFolders(workspaceId);
   return (
     <Button
-      //   onClick={onCreateNewFolder}
+      onClick={onCreateNewFolder}
       className="bg-[#1D1D1D] text-[#707070] flex items-center gap-2 py-6 px-4 rounded-2xl"
     >
       <FolderPlusDuotine />
