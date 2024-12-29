ca91a059efb4b47a38ebc4baf1b2dd3bc179cd91
s3xmaxman <badside3919@gmail.com>
2024-12-29 18:16:28 +0900

    Add getPaymentInfo function and create VideoPage component for video previews

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -171,3 +171,33 @@ export const searchUsers = async (query: string) => {
     return { status: 500, data: undefined };
   }
 };
+
+export const getPaymentInfo = async () => {
+  try {
+    const user = await currentUser();
+
+    if (!user) return { status: 404 };
+
+    const payment = await client.user.findUnique({
+      where: {
+        clerkid: user.id,
+      },
+      select: {
+        subscription: {
+          select: {
+            plan: true,
+          },
+        },
+      },
+    });
+
+    if (payment) {
+      return { status: 200, data: payment };
+    }
+
+    return { status: 400 };
+  } catch (error) {
+    console.log(error);
+    return { status: 400 };
+  }
+};
