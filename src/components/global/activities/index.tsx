2f7035670ff32bd5ac4c0087f95e5d99593dac1b
s3xmaxman <badside3919@gmail.com>
2024-12-29 22:54:58 +0900

    Refactor Activities component to enhance comment display and user interaction
    - Added 'use client' directive for client-side rendering.
    - Integrated CommentForm for submitting comments directly within the Activities component.
    - Updated rendering logic to display comments using CommentCard, improving the user experience.
    - Utilized TabsContent for better organization of activity-related content.

--- src/components/global/activities/index.tsx
+++ src/components/global/activities/index.tsx
@@ -1,4 +1,8 @@
-// import CommentCard from "../comment-card";
+"use client";
+import CommentForm from "@/components/forms/comment-form";
+import { TabsContent } from "@/components/ui/tabs";
+import React from "react";
+import CommentCard from "../comment-card";
 import { useQueryData } from "@/hooks/useQueryData";
 import { getVideoComments } from "@/actions/user";
 import { VideoCommentProps } from "@/types/index.type";
@@ -15,7 +19,26 @@ const Activities = ({ author, videoId }: Props) => {
 
   const { data: comments } = data as VideoCommentProps;
 
-  return <div>Activities</div>;
+  return (
+    <TabsContent value="Activity" className="rounded-xl flex flex-col gap-y-5">
+      <CommentForm author={author} videoId={videoId} />
+      {comments?.map((comment) => (
+        <CommentCard
+          comment={comment.comment}
+          key={comment.id}
+          author={{
+            image: comment.User?.image!,
+            firstname: comment.User?.firstname!,
+            lastname: comment.User?.lastname!,
+          }}
+          videoId={videoId}
+          reply={comment.reply}
+          commentId={comment.id}
+          createdAt={comment.createdAt}
+        />
+      ))}
+    </TabsContent>
+  );
 };
 
 export default Activities;
