c67446678642498318583c28d9badcb4429d2d7d
s3xmaxman <badside3919@gmail.com>
2024-12-29 18:35:14 +0900

    Refactor GlobalHeader component to improve header title logic
    - Introduced a new `getHeaderTitle` function to streamline title determination based on the current pathname.
    - Simplified the rendering of the header title, enhancing code readability and maintainability.

---
+++ src/app/dashboard/[workspaceId]/notifications/page.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { getNotifications } from "@/actions/user";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { useQueryData } from "@/hooks/useQueryData";
+import { QueryClient } from "@tanstack/react-query";
+import { User } from "lucide-react";
+
+const Notifications = () => {
+  const { data: notifications } = useQueryData(
+    ["user-notifications"],
+    getNotifications
+  );
+
+  const { data: notification, status } = notifications as {
+    status: number;
+    data: {
+      notification: {
+        id: string;
+        userId: string | null;
+        content: string;
+      }[];
+    };
+  };
+
+  if (status !== 200) {
+    return (
+      <div className="flex items-center justify-center h-full w-full">
+        <p>No Notifications</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col">
+      {notification.notification.map((n) => (
+        <div
+          key={n.id}
+          className="border-2 flex gap-x-3 items-center rounded-lg p-3"
+        >
+          <Avatar>
+            <AvatarFallback>
+              <User />
+            </AvatarFallback>
+          </Avatar>
+          <p>{n.content}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Notifications;
