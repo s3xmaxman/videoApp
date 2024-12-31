921f2e55f6db1eb4ff0be7eac87040c777ae072d
s3xmaxman <badside3919@gmail.com>
2024-12-31 19:14:00 +0900

    Integrate @tanstack/react-query for data fetching and enhance app structure
    - Added `@tanstack/react-query` version `5.62.11` to `package.json` and `package-lock.json` for improved data management and fetching capabilities.
    - Refactored `App.tsx` to utilize `QueryClientProvider` for managing query clients and improved component structure by integrating `ControlLayout` and `Toaster`.
    - Updated `main.tsx` to include `ClerkProvider` for enhanced authentication handling, ensuring the presence of the publishable key.
    - Enhanced utility functions in `utils.ts` with a new `onCloseApp` function for better application control.

--- desktop/src/main.tsx
+++ desktop/src/main.tsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ClerkProvider } from "@clerk/clerk-react";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
+  </React.StrictMode>
+);
 
 // Use contextBridge
-window.ipcRenderer.on('main-process-message', (_event, message) => {
-  console.log(message)
-})
+window.ipcRenderer.on("main-process-message", (_event, message) => {
+  console.log(message);
+});
