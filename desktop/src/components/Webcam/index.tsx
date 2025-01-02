372f6b4e7113f858e566055b01f5be62bf455e6c
s3xmaxman <badside3919@gmail.com>
2025-01-03 06:06:43 +0900

    Implement Webcam component with video streaming functionality
    - Refactored the Webcam component to utilize the browser's media devices for real-time video and audio streaming.
    - Introduced useRef and useEffect hooks to manage the video element and initiate the webcam stream on component mount.
    - Enhanced the video element's styling for a better user interface experience.

--- desktop/src/components/Webcam/index.tsx
+++ desktop/src/components/Webcam/index.tsx
@@ -1,5 +1,29 @@
-const Webcam = () => {
-  return <div>Webcam</div>;
+import { useEffect, useRef } from "react";
+
+const WebCam = () => {
+  const camElement = useRef<HTMLVideoElement | null>(null);
+  const streamWebCam = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+
+    if (camElement.current) {
+      camElement.current.srcObject = stream;
+      await camElement.current.play();
+    }
+  };
+
+  useEffect(() => {
+    streamWebCam();
+  }, []);
+
+  return (
+    <video
+      ref={camElement}
+      className="h-screen draggable object-cover rounded-full aspect-video border-2 relative border-white"
+    ></video>
+  );
 };
 
-export default Webcam;
+export default WebCam;
