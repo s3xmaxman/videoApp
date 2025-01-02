2c599465f2b8c17346ae270096c93a7758605e38
s3xmaxman <badside3919@gmail.com>
2025-01-03 03:40:41 +0900

    Enhance media recording capabilities with Socket.IO integration
    - Added `socket.io-client` dependency to facilitate real-time communication during media recording.
    - Refactored `recorder.ts` to include Socket.IO for improved data handling and user interaction.
    - Updated media source selection logic to support new streaming capabilities, enhancing the overall recording experience.
    - Improved variable naming for clarity and consistency in the codebase.

--- desktop/src/lib/recorder.ts
+++ desktop/src/lib/recorder.ts
@@ -1,7 +1,8 @@
 import { v4 as uuid } from "uuid";
 import { hidePluginWindow } from "./utils";
+import io from "socket.io-client";
 
-let useId: string;
+let userId: string;
 let videoTransferFileName: string | undefined;
 let mediaRecorder: MediaRecorder;
 
@@ -15,4 +16,65 @@ export const startRecording = (onSources: {
   mediaRecorder.start(1000);
 };
 
-export const onStopRecording = () => mediaRecorder.stop();
+export const onStopRecording = () => {
+  mediaRecorder.stop();
+};
+
+export const onDataAvailable = (event: BlobEvent) => {};
+
+export const selectSources = async (
+  onSources: {
+    screen: string;
+    audio: string;
+    id: string;
+    preset: "HD" | "SD";
+  },
+  videoElement: React.RefObject<HTMLVideoElement>
+) => {
+  if (onSources && onSources.screen && onSources.audio && onSources.id) {
+    const constraints: any = {
+      audio: false,
+      video: {
+        mandatory: {
+          chromeMediaSource: "desktop",
+          chromeMediaSourceId: onSources?.screen,
+          minWidth: onSources?.preset === "HD" ? 1920 : 1280,
+          maxWidth: onSources?.preset === "HD" ? 1920 : 1280,
+          minHeight: onSources?.preset === "HD" ? 1080 : 720,
+          maxHeight: onSources?.preset === "HD" ? 1080 : 720,
+          frameRate: 30,
+        },
+      },
+    };
+
+    userId = onSources.id;
+
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia(constraints);
+      const audioStream = await navigator.mediaDevices.getUserMedia({
+        video: false,
+        audio: {
+          deviceId: { exact: onSources.audio },
+        },
+      });
+
+      if (videoElement && videoElement.current) {
+        videoElement.current.srcObject = stream;
+        await videoElement.current.play();
+      }
+
+      const combinedStream = new MediaStream([
+        ...stream.getTracks(),
+        ...audioStream.getTracks(),
+      ]);
+
+      if (mediaRecorder) {
+        mediaRecorder.stream.getTracks().forEach((track) => track.stop());
+      }
+
+      mediaRecorder = new MediaRecorder(combinedStream, {
+        mimeType: "video/webm; codecs=vp9",
+      });
+    } catch (error) {}
+  }
+};
